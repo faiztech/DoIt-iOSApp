8ba92b3496b7f6280c7e56d561d70c8789b6ed5c
Mohammed Faiz <mfaizudd@neiu.edu>
2017-09-17 13:08:11 -0500

    completed screen 1

---
+++ task.swift
@@ -0,0 +1,17 @@
+//
+//  task.swift
+//  DoIt
+//
+//  Created by Mohammed Faizuddin on 9/17/17.
+//  Copyright © 2017 Mohammed Faizuddin. All rights reserved.
+//
+
+import Foundation
+
+
+class Task{
+    
+    var name = ""
+    var important = false
+    
+}
